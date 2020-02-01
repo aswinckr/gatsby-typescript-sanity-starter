6763f70503147b9a407fc5262d7ce0b73810c76e
Aswin <aswinckr@gmail.com>
2020-02-01 12:14:11 +0800

    Updated Packages

---
+++ src/pages/work.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import Image from '../components/image';
+
+import Layout from '../components/layout';
+// import ThemedLayout from '../components/theme';
+
+import SEO from '../components/seo';
+
+const Work = () => (
+  <Layout>
+    <SEO title="Work" keywords={[`work`, `portfolio`, `react`]} />
+    <h1>This is where I showcase my work</h1>
+    <Link to="/about/">Go to page 2</Link>
+  </Layout>
+);
+
+export default Work;
