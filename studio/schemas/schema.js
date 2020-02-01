e47ab4a610d7e447ecd34fe2fead2feab0b8fa79
Aswin <aswinckr@gmail.com>
2020-02-01 15:01:00 +0800

    Sanity plug in Gatsby

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -6,10 +6,9 @@ import schemaTypes from "all:part:@sanity/base/schema-type";
 
 // We import object and document schemas
 import blockContent from "./blockContent";
-import category from "./category";
-import post from "./post";
-
-import author from "./author";
+import blog from "./blog";
+import work from "./work";
+import about from "./about";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -20,9 +19,10 @@ export default createSchema({
   types: schemaTypes.concat([
     // The following are document types which will appear
     // in the studio.
-    post,
-    author,
-    category,
+    blog,
+    work,
+    about,
+
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
     blockContent
